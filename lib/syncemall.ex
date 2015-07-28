aac72350cd8038144f9b024dd4815fbb85675a11
Michał Lipski <michal@tallica.pl>
2015-07-28 23:09:23 +0200

    Match new files based on event and extension

--- lib/syncemall.ex
+++ lib/syncemall.ex
@@ -5,7 +5,21 @@ defmodule SyncEmAll do
     IO.puts "STOP"
   end
 
+  def match_events(events, match \\ [:created, :renamed]) do
+    Enum.any? events, fn event ->
+      Enum.member?(match, event)
+    end
+  end
+
+  def match_exts(filename, pattern \\ ~r/\.[a-zA-Z0-9]{2,4}$/) do
+    Regex.match? pattern, filename
+  end
+
   def callback(file_path, events) do
     IO.inspect {file_path, events}
+
+    if match_events(events) && match_exts(Path.basename(file_path)) do
+      IO.puts "Matched file: " <> file_path
+    end
   end
 end
