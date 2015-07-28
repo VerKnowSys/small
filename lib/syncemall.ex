ce131c8fada9481bc19d01b9551bbc65a6f823c1
Michał Lipski <michal@tallica.pl>
2015-07-28 21:28:15 +0200

    Add ExFSWatch dep and make use of simple usage example

--- lib/syncemall.ex
+++ lib/syncemall.ex
@@ -1,2 +1,11 @@
 defmodule SyncEmAll do
+  use ExFSWatch, dirs: [System.get_env("HOME") <> "/Pictures/Screenshots"]
+
+  def callback(:stop) do
+    IO.puts "STOP"
+  end
+
+  def callback(file_path, events) do
+    IO.inspect {file_path, events}
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SyncEmAll.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :exfswatch]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule SyncEmAll.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:exfswatch, git: "https://github.com/falood/exfswatch.git", tag: "v0.1.0"}]
   end
 end
