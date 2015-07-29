34b3a03eb93a74607f99d23094e50a4e7adc1722
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-07-29 15:44:55 +0200

    Defined basic SyncSupervisor (core launcher)

---
+++ lib/sync_supervisor.ex
@@ -0,0 +1,27 @@
+defmodule SyncSupervisor do
+  use Supervisor
+
+
+  def main [] do
+    start :normal, []
+  end
+
+
+  def start :normal, [] do
+    start_link
+  end
+
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], [{:name, __MODULE__}])
+  end
+
+  # supervisor callback
+  def init([]) do
+    children = [
+      worker(Sftp, [], []),
+    ]
+    supervise children, [strategy: :one_for_one, max_restarts: 1, max_seconds: 5]
+  end
+
+end
