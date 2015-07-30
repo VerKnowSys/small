557cfe1830f74ea6775cfcd3590d1403d539fd50
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-07-30 11:33:31 +0200

    Some code beautify in Clipboard.

--- lib/clipboard.ex
+++ lib/clipboard.ex
@@ -1,13 +1,16 @@
 defmodule Clipboard do
 
-def put text do
-  case System.cmd "sh", ["-c", "echo \"$0\" | pbcopy", "#{text}"] do
-    {results, 0} ->
-      {:ok, "Copied successfully"}
+  @doc """
+  Puts text given as param to clipboard. Works on OSX only (for now)
+  """
+  def put text do
+    case System.cmd "sh", ["-c", "echo \"$0\" | pbcopy", "#{text}"] do
+      {results, 0} ->
+        {:ok, "Copied successfully"}
 
-    {_, res} ->
-      {:error, "Can't copy to clipboard. Error: #{res}"}
+      {_, res} ->
+        {:error, "Can't copy to clipboard. Error: #{res}"}
+    end
   end
-end
 
 end
