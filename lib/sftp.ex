99f6da10ffd67684412e7b2f01d28c20794c464e
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-03 14:20:19 +0200

    Use ConfigAgent in Sftp. Cleaned hardcodes.

--- lib/sftp.ex
+++ lib/sftp.ex
@@ -7,12 +7,6 @@ defmodule Sftp do
   alias :timer, as: Timer
 
 
-  def hostname, do: "verknowsys.com"
-  def username, do: "dmilith"
-  def ssh_key_pass, do: ""
-  def port, do: 60022
-
-
   ## Client API
   def start_link opts \\ [] do
     GenServer.start_link __MODULE__, :ok, [name: __MODULE__] ++ opts
@@ -31,7 +25,11 @@ defmodule Sftp do
   ## Callbacks (Server API)
   def init :ok do
     SSH.start
-    connection = SSH.connect String.to_char_list(hostname), port, [user: String.to_char_list(username), user_interaction: false, rsa_pass_phrase: String.to_char_list(ssh_key_pass), silently_accept_hosts: true, connect_timeout: 5000]
+    config = ConfigAgent.get System.get_env "USER"
+    unless config do
+      raise "Not configured properly"
+    end
+    connection = SSH.connect String.to_char_list(config[:hostname]), config[:port], [user: String.to_char_list(config[:username]), user_interaction: false, rsa_pass_phrase: String.to_char_list(config[:ssh_key_pass]), silently_accept_hosts: true, connect_timeout: 5000]
 
     case connection do
       {:ok, conn} ->
@@ -43,8 +41,6 @@ defmodule Sftp do
         {:error, err}
     end
   end
-
-
 
 
   def send_file ssh_connection, local_file, remote_dest_file, random_uuid do
@@ -86,11 +82,12 @@ defmodule Sftp do
 
   def handle_call :add, _from, ssh_connection do
     time = Timer.tc fn ->
+      user = System.get_env "USER"
+      config = ConfigAgent.get user
       if QueueAgent.get_all > 1 do
         Logger.debug "More than one entry found in QueueAgent, merging results"
-        config = ConfigAgent.get System.get_env "USER"
         unless config do
-          raise "Unknown user #{username} for ConfigAgent. Define your user and settings first!"
+          raise "Unknown user #{user} for ConfigAgent. Define your user and settings first!"
         end
         QueueAgent.get_all
           |> Enum.map(fn elem ->
@@ -104,7 +101,7 @@ defmodule Sftp do
         case element do
           {:add, local_file, remote_dest_file, random_uuid} ->
             if File.exists?(local_file) and File.regular?(local_file) do
-              Logger.info "Handling asynchronous task to put file: #{local_file} to remote: #{hostname}:#{remote_dest_file}"
+              Logger.info "Handling asynchronous task to put file: #{local_file} to remote: #{config[:hostname]}:#{remote_dest_file}"
               Task.async fn ->
                 inner = Timer.tc fn ->
                   send_file ssh_connection, local_file, remote_dest_file, random_uuid
