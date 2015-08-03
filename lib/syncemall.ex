c5e04ef6d0c09c39eb14372c16ccc4691ad97cc3
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-03 14:07:25 +0200

    Use ConfigAgent in SyncEmAll.

--- lib/syncemall.ex
+++ lib/syncemall.ex
@@ -27,21 +27,20 @@ defmodule SyncEmAll do
     if match_events(events) && match_exts(Path.basename(file_path)) do
       Logger.info "Matched file: " <> file_path
       username = System.get_env "USER"
-      dest_name = UUID.uuid4
-      case username do
-        "dmilith" ->
-          link = "http://s.verknowsys.com/#{dest_name}.png"
-          remote_dest_file = "/home/#{username}/Web/Public/Sshots/#{dest_name}.png"
-          Clipboard.put link
-          Logger.info "Link copied to clipboard: #{link}"
-          Logger.debug "Adding an element to queue (#{file_path} -> #{remote_dest_file})"
-          QueueAgent.put {:add, file_path, remote_dest_file}
-          Sftp.add
-          Notification.send "Link synchronized: #{link}"
-
-        _ ->
-          Logger.error "You should define destination for your user ;P"
+      random_uuid = UUID.uuid4
+      config = ConfigAgent.get username
+      unless config do
+        raise "Unknown user #{username} for ConfigAgent. Define your user and settings first!"
       end
+      link = "#{config[:address]}#{random_uuid}.png"
+      remote_dest_file = "#{config[:remote_path]}#{random_uuid}.png"
+
+      QueueAgent.put {:add, file_path, remote_dest_file, random_uuid}
+      Clipboard.put link
+      Logger.info "Link copied to clipboard: #{link}"
+      Logger.debug "Adding an element to queue (#{file_path} -> #{remote_dest_file})"
+      Sftp.add
+      Notification.send "Link synchronized: #{link}"
     end
   end
 
