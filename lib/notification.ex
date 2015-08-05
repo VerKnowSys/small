b2dee7fc45769c69c693be1b802aa873acfdf573
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-05 02:05:04 +0200

    Handle case of running OSX notifications from tmux session.

--- lib/notification.ex
+++ lib/notification.ex
@@ -1,4 +1,6 @@
 defmodule Notification do
+  require Logger
+
 
   @doc """
   Sends notification using native OSX Notification Center
@@ -11,12 +13,26 @@ defmodule Notification do
   """
   @spec send(message :: String.t) :: :ok | :error
   def send message do
-    case System.cmd "osascript", ["-e", "display notification \"#{message}\" sound name \"Default\" with title \"SyncEmAll\""] do
-      {_, 0} ->
-        :ok
+    # NOTE: Using - https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
+    reattach_helper = File.exists? "/usr/local/bin/reattach-to-user-namespace"
+    case reattach_helper do
+      true ->
+        case System.cmd "/usr/local/bin/reattach-to-user-namespace", ["/usr/bin/osascript", "-e", "display notification \"#{message}\" sound name \"Default\" with title \"SyncEmAll\""] do
+          {_, 0} ->
+            :ok
 
-      {_, _} ->
-        :error
+          {_, _} ->
+            :error
+        end
+
+      false ->
+        case System.cmd "/usr/bin/osascript", ["-e", "display notification \"#{message}\" sound name \"Default\" with title \"SyncEmAll\""] do
+          {_, 0} ->
+            :ok
+
+          {_, _} ->
+            :error
+        end
     end
   end
 
