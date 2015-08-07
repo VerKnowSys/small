cc56a048c78bb3dc2f53252dfd2864cb2a352ad1
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-07 12:56:53 +0200

    Moved whole abs path to config.

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,10 @@
 use Mix.Config
 
-# project directory - used to help find out helper when running in escript mode:
-config :fs, :project_dir, (to_char_list "/Projects/small/")
 
 # path to be watched for file events:
 config :fs, :path, System.get_env("HOME") <> "/Pictures/Screenshots"
+# absolute path to events listener executable
+config :fs, :events_helper, "/Projects/small/deps/fs/priv/mac_listener"
 
 config :small, :sftp_buffer_size, 131_072
 config :small, :fs_check_interval, 30_000
