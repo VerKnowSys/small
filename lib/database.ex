a3feb586d55477ffd9b0c2105e4d29cc550a02ba
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-12 12:55:39 +0200

    Defined schema and API for Amnesia.

---
+++ lib/database.ex
@@ -0,0 +1,33 @@
+use Amnesia
+
+
+defdatabase Database do
+  # this is just a forward declaration of the table, otherwise you'd have
+  # to fully scope User.read in History functions
+  deftable User
+
+
+  deftable History, [:user_id, :content, :timestamp], type: :bag do
+    @type t :: %History{user_id: integer, content: String.t, timestamp: String.t}
+
+    # this defines a helper function to fetch the user from a History record
+    def user self do
+      User.read self.user_id
+    end
+
+  end
+
+
+  deftable User, [{ :id, autoincrement }, :name], type: :set, index: [:name] do
+    @type t :: %User{id: non_neg_integer, name: String.t}
+
+    def add_history self, content do
+      %History{user_id: self.id, content: content, timestamp: Timestamp.now} |> History.write
+    end
+
+    def histories self do
+      History.read self.id
+    end
+
+  end
+end
---
+++ lib/db.ex
@@ -0,0 +1,107 @@
+defmodule DB do
+  require Lager
+  import Lager
+  use Database
+  use Amnesia
+
+
+  def init_and_start do
+    case Amnesia.Schema.create do
+      :ok ->
+        info "Amnesia Schema created"
+
+      {:error, {_, {:already_exists, node}}} ->
+        debug "Amnesia schema already created for node: #{node}"
+
+      {:error, {_, {err, node}}} ->
+        critical "Amnesia schema cannot be created for node: #{node} cause: #{inspect err}"
+    end
+
+    notice "Starting Amnesia"
+    Amnesia.start
+
+    case Database.create disk!: [node] do
+      [:ok, :ok, :ok] ->
+        notice "Database created for node #{inspect node}"
+        Amnesia.transaction do
+          notice "Adding default user"
+          %User{name: Cfg.user} |> User.write
+        end
+
+      [error: {:already_exists, _}, error: {:already_exists, _}, error: {:already_exists, _}] ->
+        debug "Database already created"
+
+      [error: {:bad_type, _, :disc_only_copies, _}, error: {:bad_type, _, :disc_only_copies, _}, error: {:bad_type, _, :disc_only_copies, _}] ->
+        warning "Found an issue with bad_type of requested - disk_only mode. Recovering.."
+        destroy
+        init_and_start
+
+      [error: err, error: err, error: err] ->
+        critical "Database creation failure: #{inspect err}"
+
+      err ->
+        critical "Database creation failure: #{inspect err}"
+    end
+
+    # time = Timer.tc fn ->
+    #   max = 50_000
+    #   info "Adding #{max} user records"
+    #   Amnesia.transaction do
+    #     for i <- 0..max do
+    #       user = %User{name: "John#{i}"} |> User.write
+    #       user |> User.add_history "History of user #{i}"
+    #     end
+    #   end
+    # end
+
+    # case time do
+    #   {_elapsed, _} ->
+    #     notice "Add user records done in: #{_elapsed/1000}ms"
+    #     :ok
+    # end
+  end
+
+
+  defp destroy_schema do
+    warning "Stopping and destroying Amnesia schema"
+    Amnesia.stop
+    Amnesia.Schema.destroy
+  end
+
+
+  def destroy do
+    warning "Destroying database"
+    Database.destroy
+    destroy_schema
+  end
+
+
+  def close do
+    warning "Shutting down"
+    Amnesia.stop
+  end
+
+
+  def add link do
+    Amnesia.transaction do
+      selection = User.where name == Cfg.user
+      user = selection |> Amnesia.Selection.values |> List.first
+      user |> User.add_history link
+    end
+  end
+
+
+  def get_history do
+    Amnesia.transaction do
+      selection = User.where name == Cfg.user
+      user = selection |> Amnesia.Selection.values |> List.first
+
+      all_user_history = History.where user_id == user.id
+      all_user_history |> Amnesia.Selection.values |> Enum.map fn entry ->
+        "#{entry.timestamp} - #{entry.content}"
+      end
+    end
+  end
+
+
+end
