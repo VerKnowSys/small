3d8a44e70af0400865869775e32da0c4d4b356aa
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-11-22 14:54:46 +0100

    Version bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Small.Mixfile do
   def project do
     [
       app: :small,
-      version: "0.9.1",
+      version: "0.9.2",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
