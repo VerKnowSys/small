e862cfcb294040e0c35b12a11ab5c0be201df609
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-02-17 22:34:30 +0100

    new: Simplified Database model to only Queue and History.

--- lib/database.ex
+++ lib/database.ex
@@ -4,64 +4,15 @@ use Amnesia
 defdatabase Database do
   # this is just a forward declaration of the table, otherwise you'd have
   # to fully scope User.read in History functions
-  deftable User
 
 
-  deftable History, [:user_id, :content, :timestamp, :file, :uuid], type: :bag, index: [:content, :timestamp] do
-    @type t :: %History{user_id: integer, content: String.t, timestamp: String.t, file: String.t, uuid: String.t}
-
-    # this defines a helper function to fetch the user from a History record
-    def user self do
-      User.read self.user_id
-    end
-
+  deftable History, [:content, :timestamp, :file, :uuid], type: :bag, index: [:timestamp] do
+    @type t :: %History{content: String.t, timestamp: Integer.t, file: String.t, uuid: String.t}
   end
 
 
-  deftable Queue, [:user_id, :local_file, :remote_file, :uuid], type: :bag do
-    @type t :: %Queue{user_id: integer, local_file: String.t, remote_file: String.t, uuid: String.t}
-
-    def user self do
-      User.read self.user_id
-    end
-  end
-
-
-  deftable User, [{ :id, autoincrement }, :name], type: :set, index: [:name] do
-    @type t :: %User{id: non_neg_integer, name: String.t}
-
-
-    def add_history _self, history do
-      history |> History.write
-    end
-
-
-    def histories self do
-      hist = History.read self.id
-      case hist do
-        nil ->
-          []
-
-        hists ->
-          hists
-      end
-    end
-
-
-    def add_to_queue _self, record do
-      record |> Queue.write
-    end
-
-
-    def remove_from_queue _self, record do
-      record |> Queue.delete
-    end
-
-
-    def queue self do
-      Queue.read self.id
-    end
-
+  deftable Queue, [:local_file, :remote_file, :uuid], type: :bag do
+    @type t :: %Queue{local_file: String.t, remote_file: String.t, uuid: String.t}
   end
 
 
