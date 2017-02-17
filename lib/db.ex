b72ed84c5cfc9d1905678fdcd3a8c5cf88ab109e
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-02-17 22:34:58 +0100

    new: Rewritten DB helpers to match new model.

--- lib/db.ex
+++ lib/db.ex
@@ -3,8 +3,9 @@ defmodule DB do
   Database Backend module.
   """
 
-  require Lager
-  import Lager
+  require Logger
+  require Exquisite
+  alias Database
   use Database
   use Amnesia
 
@@ -13,61 +14,63 @@ defmodule DB do
     Amnesia.stop
     case Amnesia.Schema.create do
       :ok ->
-        notice "Amnesia Schema created"
+        Logger.info "Amnesia Schema created"
 
       {:error, {_, {:already_exists, a_node}}} ->
-        notice "Amnesia schema already created for node: #{a_node}"
+        Logger.info "Amnesia schema already created for node: #{a_node}"
 
       {:error, {_, {err, a_node}}} ->
-        critical "Amnesia schema cannot be created for a_node: #{a_node} cause: #{inspect err}"
+        Logger.error "CRIT: Amnesia schema cannot be created for a_node: #{a_node} cause: #{inspect err}"
 
       _ ->
-        notice "Wildcard!!"
-    end
-  end
-
-
-  defp commit_creation a_node do
-    notice "Database created for node #{inspect a_node}"
-    Amnesia.transaction do
-      notice "Adding default user"
-      %User{name: Cfg.user} |> User.write
+        Logger.info "Wildcard!!"
     end
   end
 
 
   defp create_node_if_necessary do
-    case Database.create disk!: [node] do
-      [:ok, :ok, :ok, :ok] ->
-        debug "Creating node: #{inspect node}"
-        commit_creation node
+    case Database.create disk!: [node()] do
+      [
+        :ok,
+        :ok,
+        :ok,
+      ] ->
+        Logger.debug "Creating node: #{inspect node()}"
+        Logger.info "Database created for node #{inspect node()}"
 
-      [error: {:already_exists, _}, error: {:already_exists, _}, error: {:already_exists, _}, error: {:already_exists, _}] ->
-        debug "Database already created."
+      [
+        error: {:already_exists, _},
+        error: {:already_exists, _},
+        error: {:already_exists, _},
+      ] ->
+        Logger.debug "Database already created."
 
-      [error: {:bad_type, _, :disc_only_copies, _}, error: {:bad_type, _, :disc_only_copies, _}, error: {:bad_type, _, :disc_only_copies, _}, error: {:bad_type, _, :disc_only_copies, _}] ->
-        critical "Found an issue with bad_type of requested - disk_only mode. Recovering.."
+      [
+        error: {:bad_type, _, :disc_only_copies, _},
+        error: {:bad_type, _, :disc_only_copies, _},
+        error: {:bad_type, _, :disc_only_copies, _}
+      ] ->
+        Logger.error "CRIT: Found an issue with bad_type of requested - disk_only mode. Recovering.."
         # destroy
         # init_and_start
 
       err ->
-        critical "Database creation failure: #{inspect err}"
+        Logger.error "CRIT: Database creation failure: #{inspect err}"
     end
   end
 
 
   def init_and_start do
-    Amnesia.stop
-    File.mkdir_p Cfg.project_dir
-    create_amnesia_schema
-    Amnesia.start
-    debug "Schema dump:\n#{Amnesia.Schema.print}"
-    create_node_if_necessary
+    Amnesia.stop()
+    File.mkdir_p Cfg.project_dir()
+    create_amnesia_schema()
+    Amnesia.start()
+    create_node_if_necessary()
   end
 
 
   def dump_mnesia param \\ "" do
-    File.mkdir_p Cfg.mnesia_dumps_dir
+    File.mkdir_p Cfg.mnesia_dumps_dir()
     case param do
       "" ->
         dump_db_file Timestamp.now |> (String.replace ~r/[:. ]/, "-")
@@ -79,39 +82,40 @@ defmodule DB do
 
   def dump_db_file tstamp do
     dump_name = "#{Cfg.mnesia_dumps_dir}mnesia-db.#{tstamp}.erl"
-    notice "Dumping database to file: #{dump_name}"
+    Logger.info "Dumping database to file: #{dump_name}"
     Amnesia.dump "#{dump_name}"
   end
 
 
   defp destroy_schema do
-    warning "Stopping and destroying Amnesia schema"
-    Amnesia.stop
-    Amnesia.Schema.destroy
+    Logger.warn " Stopping and destroying Amnesia schema"
+    Amnesia.stop()
+    Amnesia.Schema.destroy()
   end
 
 
   def destroy do
-    warning "Destroying database"
-    list = Database.destroy
-    debug "Destroyed: #{inspect list}"
-    destroy_schema
+    Logger.warn " Destroying database"
+    list = Database.destroy()
+    Logger.debug "Destroyed: #{inspect list}"
+    destroy_schema()
   end
 
 
   def close do
-    warning "Shutting down"
+    Logger.warn " Shutting down"
     Amnesia.stop
   end
 
 
-  @doc """
-  Helper to get user struct from Mnesia
-  """
-  def user do
-    Amnesia.transaction do
-      selection = User.where name == Cfg.user
-      selection |> Amnesia.Selection.values |> List.first
+  def histories do
+    hist = Amnesia.Selection.values History.where timestamp != -1
+    case hist do
+      nil ->
+        []
+
+      hists ->
+        hists
     end
   end
 
@@ -121,7 +125,7 @@ defmodule DB do
   """
   def add_history history do
     Amnesia.transaction do
-      user |> (User.add_history history)
+      History.write history
     end
   end
 
@@ -131,7 +135,7 @@ defmodule DB do
   """
   def get_queue do
     Amnesia.transaction do
-      queue = user |> User.queue
+      queue = Queue.first
       case queue do
         nil ->
           []
@@ -144,31 +148,25 @@ defmodule DB do
 
   def add_to_queue record do
     Amnesia.transaction do
-      user |> (User.add_to_queue record)
+      Queue.write record
     end
   end
 
 
   def remove_from_queue record do
     Amnesia.transaction do
-      user |> (User.remove_from_queue record)
+      Queue.delete record
     end
   end
 
 
   def get_history do
     Amnesia.transaction do
-      # debug "get_history - user: #{inspect user}, histories: #{inspect user |> User.histories |> Enum.take 5}"
-      # NOTE: slower but more expressie way:
-      # (History.where user_id == user.id)
-      # |> Amnesia.Selection.values
-      #
-      # or:
-      user |> User.histories
+      DB.histories()
         |> (Enum.sort fn e1, e2 -> e1.timestamp > e2.timestamp end)
         |> (Enum.map fn entry ->
-                %Database.History{user_id: entry.user_id, timestamp: entry.timestamp, content: entry.content, file: entry.file, uuid: entry.uuid}
-              end)
+          %Database.History{timestamp: entry.timestamp, content: entry.content, file: entry.file, uuid: entry.uuid}
+        end)
     end
   end
 
