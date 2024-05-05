ab3a1c625ce7d19bc08881285f867af63b93ca2e
Daniel (dmilith) Dettlaff <dmilith@me.com>
2024-05-05 21:06:29 +0200

    new: Formatted mix.exs. Use the last available Amnesia version: v0.2.8

--- mix.exs
+++ mix.exs
@@ -6,28 +6,25 @@ defmodule Small.Mixfile do
       app: :small,
       version: "0.14.0",
       elixir: "~> 1.16",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: escript(),
       dialyzer: [
         paths: [
-          "_build/dev/lib/small/ebin",
-        ],
+          "_build/dev/lib/small/ebin"
+        ]
       ]
     ]
   end
-
 
   def application do
     [extra_applications: [:logger, :uuid, :fs, :amnesia, :cowboy, :ssh]]
   end
 
-
   defp mixenv do
-    Atom.to_string Mix.env
+    Atom.to_string(Mix.env())
   end
-
 
   def escript do
     [
@@ -36,10 +33,9 @@ defmodule Small.Mixfile do
       language: :elixir,
       force: true,
       emu_args: "-smp enable -sname small#{mixenv()}",
-      comment: "ServeD",
+      comment: "ServeD"
     ]
   end
-
 
   # def applications do
   #   dev = [:exsync, :credo]
@@ -51,7 +47,6 @@ defmodule Small.Mixfile do
   #       dev ++ prod
   #   end
   # end
-
 
   # Dependencies can be Hex packages:
   #
@@ -69,15 +64,14 @@ defmodule Small.Mixfile do
       # { :tzdata, "~> 0.5" }, # , override: :true
       # { :timex, "~> 1.0" },
 
-      { :uuid, "~> 1.1" },
-      { :cowboy, "~> 1.0" },
-
-      { :amnesia, github: "meh/amnesia", branch: "master" },
-      { :exquisite, github: "meh/exquisite", branch: "master", override: true },
-      { :fs, github: "VerKnowSys/fs", branch: "master", override: true },
-
-      { :credo, "~> 0.6", only: :dev },
-      { :dialyxir, "~> 1.3", only: [:dev], runtime: false },
+      {:uuid, "~> 1.1"},
+      {:cowboy, "~> 1.0"},
+      {:amnesia, "~> 0.2"},
+      # {:amnesia, github: "meh/amnesia", branch: "master"},
+      {:exquisite, github: "meh/exquisite", branch: "master", override: true},
+      {:fs, github: "VerKnowSys/fs", branch: "master", override: true},
+      {:credo, "~> 0.6", only: :dev},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
 
       # { :exsync, github: "VerKnowSys/exsync", only: :dev },
       # { :exsync, "~> 0.1", only: :dev },
