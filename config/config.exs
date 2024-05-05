7b7fe02588313bfe7bf3192b85328b6046b3ab37
Daniel (dmilith) Dettlaff <dmilith@me.com>
2024-05-05 20:32:18 +0200

    new: No more deprecations in the config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,37 +1,40 @@
-use Mix.Config
-
 mix_env = System.get_env("MIX_ENV") || "dev"
 mix_home = System.get_env("HOME") || "/tmp"
+pwd = System.get_env("PWD") || "/tmp"
 
 # Logger
-config :logger, :console, format: "$time $message\n"
+Config.config :logger, :console, format: "$time $message\n"
 
 # path to be watched for file events:
-config :fs, :path, mix_home <> "/Pictures/Screenshots"
+Config.config :fs, :path, mix_home <> "/Pictures/Screenshots"
+
 # absolute path to events listener executable
 helper_path = if File.exists?("/Users/Shared/Small/bin/mac_listener"), do: "/Users/Shared/Small/bin/mac_listener", else: pwd <> "/bin/mac_listener"
 Config.config :fs, :events_helper, to_charlist helper_path
+
+# mnesia data dir:
+Config.config :mnesia, :dir, (to_charlist mix_home <> (
   case :os.type() do
     {:unix, :darwin} -> "/Library/Small/"
     {:unix, _}       -> "/.small/"
   end
 ) <> mix_env)
 
-config :small, :amount_history_load, 16
-config :small, :webapi_port, 8000
-config :small, :webapi_dev_port, 8001
-config :small, :webapi_test_port, 8002
-config :small, :sftp_write_timeout, 15_000
-config :small, :sftp_open_channel_timeout, 15_000
-config :small, :sftp_start_channel_timeout, 15_000
-config :small, :sftp_buffer_size, 131_072
-config :small, :fs_check_interval, 2_000
-config :small, :ssh_connection_timeout, 10_000
-config :small, :mnesia_autodump_interval, 21_600_000 # 6h
+Config.config :small, :amount_history_load, 16
+Config.config :small, :webapi_port, 8000
+Config.config :small, :webapi_dev_port, 8001
+Config.config :small, :webapi_test_port, 8002
+Config.config :small, :sftp_write_timeout, 15_000
+Config.config :small, :sftp_open_channel_timeout, 15_000
+Config.config :small, :sftp_start_channel_timeout, 15_000
+Config.config :small, :sftp_buffer_size, 131_072
+Config.config :small, :fs_check_interval, 2_000
+Config.config :small, :ssh_connection_timeout, 10_000
+Config.config :small, :mnesia_autodump_interval, 21_600_000 # 6h
 
 
-# config :exlager, level: :debug
-# config :lager,
+# Config.config :exlager, level: :debug
+# Config.config :lager,
 #   [
 #     colored: true,
 #     colors: [
